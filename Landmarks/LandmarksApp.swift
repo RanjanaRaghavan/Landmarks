bd0213944e0786d8e3203a48f0944b15412b1e77
RanjanaRaghavan <ranjanaraghavan911@gmail.com>
2025-05-31 08:03:34 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ranjana Raghavan on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
